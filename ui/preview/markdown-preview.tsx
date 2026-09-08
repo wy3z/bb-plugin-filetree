44990fc1d07776a61773dee50eabcaeee029b3f1
WYEZ <jamesrhysellis@live.com>
2026-09-08 13:01:26 +0100

    Remove superseded preview component

--- ui/preview/markdown-preview.tsx
+++
@@ -1,9 +0,0 @@
-import { Markdown } from "@get-bb/plugin-sdk/app";
-
-export function MarkdownPreview(props: { content: string }) {
-  return (
-    <div className="filetree-preview-markdown">
-      <Markdown content={props.content} />
-    </div>
-  );
-}
